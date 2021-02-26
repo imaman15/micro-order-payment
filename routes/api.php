093f0e44a417e205252543788548845997ade124
imaman15 <imamagustiannugraha@ymail.com>
2021-02-27 00:04:41 +0700

    Create webhook handler

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('orders', 'OrderController@index');
+Route::post('orders', 'OrderController@create');
+
+Route::post('webhook', 'WebhookController@midtransHandler');
